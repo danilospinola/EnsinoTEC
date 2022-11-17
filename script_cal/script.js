ef0eee3616fd22767198ad8c9c5e80595754b68e
medina157 <mdnrdrgs@gmail.com>
2022-11-17 09:03:45 -0300

    calendario pop up

--- script_cal/script.js
+++ script_cal/script.js
@@ -1,3 +1,10 @@
+
+const popup = document.getElementById('popup');
+
+function handlePopup(open) {
+	popup.classList[open ? 'add' : 'remove']('opened');
+}
+
 var app = {
 	settings: {
 		container: $('.calendar'),
