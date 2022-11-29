c581a12133ca70c28140d869174c2c77d9870176
danilospinola <danspinola10@gmail.com>
2022-11-29 08:45:58 -0300

    Update module.js

--- firebase/module.js
+++ firebase/module.js
@@ -231,7 +231,7 @@ if (htmlAtual == "/EnsinoTEC/perfil-aluno.html"){
               const querySnapshot = await getDocs(q);
               querySnapshot.forEach((doc) => {
               // doc.data() is never undefined for query doc snapshots
-              console.log(doc.id, " => ", doc.data("inputNomeCompletoProf"));
+              console.log(doc.id, " => ", doc.data().inputNomeCompletoProf);
               });
 
             } else {
